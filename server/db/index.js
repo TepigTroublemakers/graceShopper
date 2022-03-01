324079b64ccf017d6a6f75e46c3221b3bfccc5f8
Evan Garris <egarris@gmail.com>
2022-03-01 17:38:36 -0600

    refactor: debugged merge changes

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,21 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require('./db');
 
-const User = require('./models/User')
+const User = require('./models/User');
+const Pot = require('./models/Pot');
+const PaymentInfo = require('./models/PaymentInfo');
 
-//associations could go here!
+User.hasMany(PaymentInfo);
+PaymentInfo.belongsTo(User);
+User.belongsToMany(Pot, { through: 'Cart' });
+Pot.belongsToMany(User, { through: 'Cart' });
 
 module.exports = {
   db,
   models: {
     User,
+    Pot,
+    PaymentInfo,
   },
-}
+};
