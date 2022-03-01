8e15df361a3fcb2221335fc69cd03331c5ac9fc3
Rob Watkins <rwatkins123@gmail.com>
2022-03-01 14:51:24 -0600

    pots api routes added

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 module.exports = router
 
+const potsRouter = require('./pots')
+
 router.use('/users', require('./users'))
+
+router.use('/pots', potsRouter)
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
