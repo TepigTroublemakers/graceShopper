1cae9fbf66da3684b759fe74b2ef234f4cf774c2
Rob Watkins <rwatkins123@gmail.com>
2022-03-02 12:08:36 -0600

    singlepot redux logic added

--- client/store/pots.js
+++ client/store/pots.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 // Set action types
 const SET_ALL_POTS = 'SET_ALL_POTS';
+const DELETE_POT = 'DELETE_POT';
+const UPDATE_POT = 'UPDATE_POT'
 
 // Action creators
 const setAllPots = (pots) => {
@@ -10,6 +12,20 @@ const setAllPots = (pots) => {
     pots,
   };
 };
+
+const _deletePot = (pot) => {
+  return {
+  type: DELETE_POT,
+  pot
+  }
+}
+
+const _updatePot = (pot) => {
+  return {
+    type: UPDATE_POT,
+    pot
+  }
+}
 
 // Thunk creators
 export const getAllPots = () => {
@@ -23,6 +39,28 @@ export const getAllPots = () => {
   };
 };
 
+export const deletePot = (id) => {
+  return async (dispatch) => {
+    try {
+      const { data: pot } = await axios.delete(`/api/pots/${id}`)
+      dispatch(_deletePot(pot))
+    } catch(error) {
+      console.error('Error in deletePot thunk!!!')
+    }
+  }
+}
+
+export const updatePot = (pot) => {
+  return async (dispatch) => {
+    try {
+    const { data: updatedPot } = await axios.put(`/api/pot/${pot.id}`, pot)
+    dispatch(_updatePot(updatedPot))
+    } catch (error) {
+      console.error('Error in updatePot thunk!')
+    }
+  }
+}
+
 // Initial state
 const initState = [];
 
@@ -31,6 +69,14 @@ const pots = (state = initState, action) => {
   switch (action.type) {
     case SET_ALL_POTS: {
       return action.pots;
+    }
+    case DELETE_POT: {
+      return state.filter((pot) => pot.id !== action.pot.id)
+    }
+    case UPDATE_POT: {
+      return state.map((pot) =>
+      pot.id === action.pot.id ? action.pot : pot
+      );
     }
     default:
       return state;
