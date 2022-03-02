0dd4436d7c26dde46239ab26b128ebc163322a32
Evan Garris <egarris@gmail.com>
2022-03-02 13:52:56 -0600

    add: more SinglePot redux logic

--- client/store/pots.js
+++ client/store/pots.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 // Set action types
 const SET_ALL_POTS = 'SET_ALL_POTS';
 const DELETE_POT = 'DELETE_POT';
-const UPDATE_POT = 'UPDATE_POT'
+const UPDATE_POT = 'UPDATE_POT';
+const ADD_SINGLE_POT = 'ADD_SINGLE_POT';
 
 // Action creators
 const setAllPots = (pots) => {
@@ -15,17 +16,24 @@ const setAllPots = (pots) => {
 
 const _deletePot = (pot) => {
   return {
-  type: DELETE_POT,
-  pot
-  }
-}
+    type: DELETE_POT,
+    pot,
+  };
+};
 
 const _updatePot = (pot) => {
   return {
     type: UPDATE_POT,
-    pot
-  }
-}
+    pot,
+  };
+};
+
+const _addSinglePot = (pot) => {
+  return {
+    type: ADD_SINGLE_POT,
+    pot,
+  };
+};
 
 // Thunk creators
 export const getAllPots = () => {
@@ -42,24 +50,35 @@ export const getAllPots = () => {
 export const deletePot = (id) => {
   return async (dispatch) => {
     try {
-      const { data: pot } = await axios.delete(`/api/pots/${id}`)
-      dispatch(_deletePot(pot))
-    } catch(error) {
-      console.error('Error in deletePot thunk!!!')
+      const { data: pot } = await axios.delete(`/api/pots/${id}`);
+      dispatch(_deletePot(pot));
+    } catch (error) {
+      console.error('Error in deletePot thunk!!!');
     }
-  }
-}
+  };
+};
 
 export const updatePot = (pot) => {
   return async (dispatch) => {
     try {
-    const { data: updatedPot } = await axios.put(`/api/pot/${pot.id}`, pot)
-    dispatch(_updatePot(updatedPot))
+      const { data: updatedPot } = await axios.put(`/api/pots/${pot.id}`, pot);
+      dispatch(_updatePot(updatedPot));
     } catch (error) {
-      console.error('Error in updatePot thunk!')
+      console.error('Error in updatePot thunk!');
     }
-  }
-}
+  };
+};
+
+export const addSinglePot = (pot) => {
+  return async (dispatch) => {
+    try {
+      const { data: createdPot } = await axios.post('/api/pots', pot);
+      dispatch(_addSinglePot(createdPot));
+    } catch (error) {
+      console.error('Error in addSinglePot thunk!!');
+    }
+  };
+};
 
 // Initial state
 const initState = [];
@@ -71,12 +90,13 @@ const pots = (state = initState, action) => {
       return action.pots;
     }
     case DELETE_POT: {
-      return state.filter((pot) => pot.id !== action.pot.id)
+      return state.filter((pot) => pot.id !== action.pot.id);
     }
     case UPDATE_POT: {
-      return state.map((pot) =>
-      pot.id === action.pot.id ? action.pot : pot
-      );
+      return state.map((pot) => (pot.id === action.pot.id ? action.pot : pot));
+    }
+    case ADD_SINGLE_POT: {
+      return [...state, action.pot];
     }
     default:
       return state;
