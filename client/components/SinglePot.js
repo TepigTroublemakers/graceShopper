557a9b99ba9a58aab8ad30cec07f1a62c5cc9bdf
Evan Garris <egarris@gmail.com>
2022-03-03 17:20:19 -0600

    add: SinglePot local cart logic (w/bugs!!)

--- client/components/SinglePot.js
+++ client/components/SinglePot.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getSinglePot } from '../store/singlePot';
@@ -10,20 +10,64 @@ const SinglePot = () => {
     };
   });
 
+  const [orderQty, setOrderQty] = useState(1);
+  const [cartData, setCartData] = useState([]);
+
   const { potId } = useParams();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getSinglePot(potId));
+    if (localStorage.data) {
+      const data = JSON.parse(localStorage.getItem('data'));
+      setCartData(data);
+    }
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem('data', JSON.stringify(cartData));
+  }, [cartData]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCartData([
+      ...cartData,
+      {
+        description: description,
+        quantity: orderQty,
+        price: price,
+      },
+    ]);
+  };
+  console.log('AFTER SUBMIT: ', cartData);
+
+  console.log('LOCAL: ', localStorage.data);
+
+  const { description, imageUrl, quantity, price, category } = singlePot;
 
   return (
     <div>
-      <img src={singlePot.imageUrl} style={{ width: '350px' }} />
-      <h2>{singlePot.description}</h2>
-      <h4>Quantity On Hand: {singlePot.quantity}</h4>
-      <h4>Price: ${singlePot.price}</h4>
-      <h4>Category: {singlePot.category}</h4>
+      <img src={imageUrl} style={{ width: '350px' }} />
+      <h2>{description}</h2>
+      <h4>Quantity On Hand: {quantity}</h4>
+      <h4>Price: ${price}</h4>
+      <h4>Category: {category}</h4>
+      <br />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="qty">Quantity:</label>
+        <input
+          type="number"
+          id="qty"
+          name="quantity"
+          value={orderQty}
+          min="1"
+          max={quantity}
+          onChange={(e) => setOrderQty(e.target.value)}
+        />
+        <br />
+        <br />
+        <button onSubmit={handleSubmit} type="submit">Add to Cart</button>
+      </form>
     </div>
   );
 };
