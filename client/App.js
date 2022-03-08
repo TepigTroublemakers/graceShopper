97f25a8691489b1d1235f010390dfc367458a90a
hujeffy1 <hujeffy.1@gmail.com>
2022-03-08 18:22:54 -0500

    Feature: PayPal Payment

--- client/App.js
+++ client/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Routes from './Routes';
 import Footer from './components/Footer';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Routes />
-      <Footer />
+      <PayPalScriptProvider
+        options={{
+          'client-id':
+            'AaGHiT6kvUSyBteicuIBmc9nGPhMshi7XgPXBI3SIj98aploed1bK-sNLTGmco8xMfrKuJaG7P9zq5_d',
+        }}
+      >
+        <Navbar />
+        <Routes />
+        <Footer />
+      </PayPalScriptProvider>
     </div>
   );
 };
---
+++ client/components/PaypalCheckoutButton.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { PayPalButtons } from '@paypal/react-paypal-js';
+
+const PaypalCheckoutButton = (props) => {
+  const { product } = props;
+  const [paidFor, setPaidFor] = useState(false);
+  const [error, setError] = useState(null);
+
+  function handleApprove(orderId) {
+    setPaidFor(true);
+    if (error) {
+      setError(
+        'Your payment was processed successfully. However, we are unable to fulfill your purchase. Please contact us at support@designcode.io for assistance.'
+      );
+    }
+  }
+
+  return (
+    <PayPalButtons
+      style={{
+        color: 'silver',
+        layout: 'vertical',
+        height: 48,
+        tagline: false,
+        shape: 'pill',
+      }}
+      createOrder={(data, actions) => {
+        return actions.order.create({
+          purchase_units: [
+            {
+              description: product.description,
+              amount: {
+                value: product.price,
+              },
+            },
+          ],
+        });
+      }}
+      onApprove={async (data, actions) => {
+        const order = await actions.order.capture();
+        console.log('order', order);
+
+        handleApprove(data.orderID);
+      }}
+      onError={(err) => {
+        setError(err);
+        console.error('PayPal Checkout onError', err);
+      }}
+      onCancel={() => {
+        props.history.push('/cart');
+      }}
+    />
+  );
+};
+
+export default PaypalCheckoutButton;
